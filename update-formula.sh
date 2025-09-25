540b797aec20e5631e7026c89258a8bce18e4d1f
Angus Mitchell <anguspmitchell@gmail.com>
2025-09-24 21:40:20 -0500

    update formula

---
+++ update-formula.sh
@@ -0,0 +1,4 @@
+curl -L https://github.com/btf-org/flank/archive/refs/heads/main.tar.gz -o /tmp/flank-main.tar.gz
+SHA=$(shasum -a 256 /tmp/flank-main.tar.gz | awk '{print $1}')
+rm /tmp/flank-main.tar.gz
+sed -i "s/sha256 \".*\"/sha256 \""$SHA"\"/" Formula/flank.rb
