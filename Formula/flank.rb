75c000a864ea0555da25a4b274a45a4de3508d87
Angus Mitchell <anguspmitchell@gmail.com>
2025-10-02 16:13:17 -0500

    sha v0.0.1

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Test formula that installs the flank script"
   homepage "https://github.com/btf-org/flank"
-  url "https://github.com/btf-org/flank/archive/refs/tags/v1.2.3.tar.gz"
-  version "v0.1.0"
+  url "https://github.com/btf-org/flank/archive/refs/tags/v0.1.0.tar.gz"
+  version "0.1.0"
   sha256 "5e98ec67cd08abe9daa1eddf8faf371209a929091f8ebaffc19249feea2f6f06"
 
 
