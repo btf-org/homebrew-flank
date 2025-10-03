aa4eedbaa5089e85f8383791a19b93fa5e16d4d4
Angus Mitchell <anguspmitchell@gmail.com>
2025-10-03 10:03:41 -0500

    install index.html and add caveats

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -16,7 +16,32 @@ class Flank < Formula
     system "gcc", "server.c", "-o", "flankserver"
     bin.install "flankserver"
 
+    pkgshare.install "index.html"
+
     (prefix/"INSTALL_LOG.txt").write("Installed at #{Time.now}")
+  end
+
+  def caveats
+    <<~EOS
+      Flank has been installed!
+      
+      If you care, there are three components:
+        1. The restricted shell, installed at
+            #{bin}/iflank
+        2. The server that chats with the shell, at
+            #{bin}/flankserver
+        3. The CLI utility, installed at
+            #{bin}/flank
+      
+      The webpage is stored at
+        #{pkgshare}/index.html
+
+      Your programs will be stored in files in
+        #{var}/flank
+
+      To get started, run `flank` and follow the prompts:
+        $ flank
+    EOS
   end
 
   def post_install
