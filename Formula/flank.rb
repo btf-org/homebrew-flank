08e769679471479088ab29818ab1e0c7943c1306
Angus Mitchell <anguspmitchell@gmail.com>
2025-10-20 12:38:29 -0500

    Update to v0.1.18

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,9 +1,9 @@
 class Flank < Formula
   desc "Test formula that installs the flank script"
   homepage "https://github.com/btf-org/flank"
-  url "https://github.com/btf-org/flank/archive/refs/tags/v0.1.17.tar.gz"
-  version "0.1.17"
-  sha256 "90d7f8d5c6eee0450dfb8c8991e29698770a62aeab56ae0ba06b270124908b9f"
+  url "https://github.com/btf-org/flank/archive/refs/tags/v0.1.18.tar.gz"
+  version "0.1.18"
+  sha256 "99a221370687fdfbe5833e2290de835778bfee7a7ad9931ec19dd562c644e64c"
 
 
 
