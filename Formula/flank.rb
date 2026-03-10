cc70f16d601eb5b2c7d3d7faffd80ef0b3af926e
Angus Mitchell <anguspmitchell@gmail.com>
2026-03-10 12:17:12 -0500

    Update to v0.1.75

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,9 +1,9 @@
 class Flank < Formula
   desc "Test formula that installs the flank script"
   homepage "https://github.com/btf-org/flank"
-  url "https://github.com/btf-org/flank/archive/refs/tags/v0.1.74.tar.gz"
-  version "0.1.74"
-  sha256 "1faa81504a176a8b69a9abf164e7e418e4f5383389f882f29e09e63862897c7a"
+  url "https://github.com/btf-org/flank/archive/refs/tags/v0.1.75.tar.gz"
+  version "0.1.75"
+  sha256 "0758a007640859e2b3eafda8d328b03f58c8da9d757c7c99202d83e7e2f3c495"
 
   def install
     bin.install "iflank" => "iflank" if File.exist?("iflank")
